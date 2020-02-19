a778d3dbccef0ff0076dfa80ab29de54ea0e8933
susmithachilkuri <46213414+susmithachilkuri@users.noreply.github.com>
2020-02-19 22:38:26 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-      withCredentials([string(credentialsId: 'aws-access-key', variable: 'AWS_ACCESS_KEY_ID'), 
-                       string(credentialsId: 'aws-secret-key', variable: 'AWS_SECRET_ACCESS_KEY')])
-      region     = "us-east-2"
-}
-
 resource "aws_instance" "example" {
-      ami          = "ami-0fc20dd1da406780b"
-      instance_type= "t2.micro"
-}   
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+}
