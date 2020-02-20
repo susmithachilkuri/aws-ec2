8afecc8ce410afc6a2cca922f5c7a2b45aa1741d
susmithachilkuri <46213414+susmithachilkuri@users.noreply.github.com>
2020-02-20 15:33:11 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,9 +1,7 @@
 variable "AWS_ACCESS_KEY" {
-  withCredentials([string(credentialsId: 'aws-access-key', variable: 'AWS_ACCESS_KEY_ID')])
 }
 
 variable "AWS_SECRET_KEY" {
- string(credentialsId: 'aws-secret-key', variable: 'AWS_SECRET_ACCESS_KEY')])
 }
 
 variable "AWS_REGION" {
